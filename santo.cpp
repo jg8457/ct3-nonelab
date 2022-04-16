057e904abe02cd026d739478b83c2aef814fcdc7
jg8457 <103809703+jg8457@users.noreply.github.com>
2022-04-16 11:33:35 +0530

    Update santo.cpp

--- santo.cpp
+++ santo.cpp
@@ -1,11 +1,11 @@
 #include <stdio.h>
 int main()
 {
-     int billamt,amtgiven;
+    int billamt,amtgiven;
     scanf("%d \n %d",&billamt,&amtgiven);
     printf("Quotient:");
     printf("%d\n",billamt/amtgiven);
     printf("Remainder:");
     printf("%d",billamt%amtgiven);
-	  return 0;
+    return 0;
 }
